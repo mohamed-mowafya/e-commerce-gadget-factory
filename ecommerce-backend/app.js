8929d0cc5a1570e740a11e55aae2eff4ac2a1261
Mohamed_Chemmi <1804233@bdeb.qc.ca>
2021-09-28 08:31:43 -0300

    Connection MongoDb Cloud
    - Modification du gitignore
    - Modification du App.js

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const app = express();
-require('dotenv').config()
+const mongoose = require('mongoose');
 
+
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+// la connexion avec la BD :
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+  )
+  .then(() => console.log('DB Marche'))
+  
+  mongoose.connection.on('error', err => {
+    console.log(`Erreur de la BD: ${err.message}`)
+  });
+  
+
+
+
+
+//Notre application :
+const app = express();
+
+
+
+
+// Les routes
 
 app.get('/',(req,res)=>{
 res.send('Test');
