e2872351fa6760d9c9d8035c356e5a4c011421b6
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-09-30 23:00:37 -0400

    Correction layout
    suppression du jumbotron

--- ecommerce-frontend/src/site/Layout.js
+++ ecommerce-frontend/src/site/Layout.js
@@ -9,19 +9,7 @@ const Layout = ({
     children}) => (
     <div>
     <Navbar/>
-    <div className="jumbotron">
-        <h2 className="">
-            {title}
-        </h2>
-
-        <p className="lead">
-            {description}
-        </p>
-
-    </div>
-    <div className={className}>
-        {children}
-        </div>
+    <div className={className}>{children}</div>
     </div>
     );
 
