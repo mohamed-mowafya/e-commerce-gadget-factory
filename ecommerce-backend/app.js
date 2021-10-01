3318c37afee67c707596d58f72447082bff1d23f
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-10-01 00:02:21 -0400

    Resolution de conflits

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -9,9 +9,7 @@ const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 
 // importation routes:
-const userRoutes = require('./routes/user')
-
-
+const authRoutes = require('./routes/auth')
 // load env variables
 const dotenv = require('dotenv');
 dotenv.config()
@@ -55,9 +53,7 @@ app.use(function(req, res, next) {
 
 
 // Routes middlewares
-
-app.use("/api",userRoutes)
-
+app.use("/api",authRoutes)
 
 const port = process.env.PORT || 8000;
 app.listen(port,()=>{
--- ecommerce-backend/routes/auth.js
+++ ecommerce-backend/routes/auth.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-
-const {signup,signin,signout,requireSignin} = require('../controllers/user');
+const {signup,signin,signout,requireSignin} = require('../controllers/auth');
 const {userSignupValidator}= require ('../validator');
 
 router.post("/signup",userSignupValidator,signup)
