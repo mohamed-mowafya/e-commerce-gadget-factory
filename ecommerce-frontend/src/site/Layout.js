d0ee948b70d6c3c1ab56a7d48542fca83663a745
ViktoryaMkr <64432981+ViktoryaMkr@users.noreply.github.com>
2021-09-30 22:14:00 -0400

    Modification Layout
    - aJout du css pour le jumbotron dans le layout

--- ecommerce-frontend/src/site/Layout.js
+++ ecommerce-frontend/src/site/Layout.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Navbar from './Navbar';
-const Layout = ({title ='title',description ='Description',className,children}) => (
+import '../CSS/Layout.css'
+
+const Layout = ({
+    title ='title',
+    description ='Description',
+    className,
+    children}) => (
     <div>
     <Navbar/>
-    <div className={className}>{children}</div>
+    <div className="jumbotron">
+        <h2 className="">
+            {title}
+        </h2>
+
+        <p className="lead">
+            {description}
+        </p>
+
     </div>
-)
+    <div className={className}>
+        {children}
+        </div>
+    </div>
+    );
+
 export default Layout;
