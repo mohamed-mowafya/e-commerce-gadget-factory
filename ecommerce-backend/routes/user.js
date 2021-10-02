5cc04c225d47258615f5b9516ba29614fe2f6b5f
Mohamed_Chemmi <1804233@bdeb.qc.ca>
2021-10-02 01:51:45 -0300

    Creation des routes middlewares Administrateur et utilisateur normal
    -Test Postman reussie

--- ecommerce-backend/routes/user.js
+++ ecommerce-backend/routes/user.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 
 const { userById} = require('../controllers/user');
-const { requireSignin} = require('../controllers/auth');
+const { requireSignin,isAdmin,isAuth} = require('../controllers/auth');
 
 
 //TEST POUR isAuth + isAdmin
-router.get('/secret/:id', requireSignin,(req, res) => {
+router.get('/secret/:id', requireSignin,isAdmin,isAuth,(req, res) => {
     res.json({
         user: 'TEST MARCHE',
         user:req.profile
