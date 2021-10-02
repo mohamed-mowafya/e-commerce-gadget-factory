6ef96b3e919d2bffa9465383481ff934e4eb2fea
Mohamed_Chemmi <1804233@bdeb.qc.ca>
2021-10-02 01:21:01 -0300

    Creation du middleware  userById
    -Fixer des bugs au niveau de quelques methodes.
    -Test Postman reussie

---
+++ ecommerce-backend/controllers/user.js
@@ -0,0 +1,15 @@
+const User = require('../Models/user');
+
+
+//Chercher un utilisateur par son ID
+exports.userById = (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'Utilisateur introuvable'
+            });
+        }
+        req.profile = user;
+        next();
+    });
+};
