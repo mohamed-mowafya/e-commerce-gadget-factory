db1f658ce29ca275d4745903acf30f5b0407655a
HamzaBelmouh <hamzabelmouh01@gmail.com>
2021-10-02 14:35:12 -0700

    Update de Category

--- ecommerce-backend/routes/category.js
+++ ecommerce-backend/routes/category.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 
 const {create} = require('../controllers/category');
+const { requireSignin,isAdmin,isAuth} = require('../controllers/auth');
+const { userById} = require('../controllers/user');
 
-router.post("/category/create",create);
+router.post(
+    "/category/create/:id",
+    requireSignin, 
+    isAuth,
+    isAdmin,
+    create
+);
+
+
+router.param('id', userById);
 
 module.exports = router;
