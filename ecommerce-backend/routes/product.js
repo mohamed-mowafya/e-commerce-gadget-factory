f797bbecd7e59797e467f70a8251b2700b8b2b62
HamzaBelmouh <hamzabelmouh01@gmail.com>
2021-10-09 14:42:47 -0700

    Creation de la methode list
    methode list qui va aller chercher les produits les plus vendu sur le site

--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const {create, productById, read, remove, update} = require('../controllers/product');
+const {create, productById, read, remove, update, list} = require('../controllers/product');
 const { requireSignin,isAdmin,isAuth} = require('../controllers/auth');
 const { userById} = require('../controllers/user');
 
@@ -10,6 +10,10 @@ router.get('/product/:productId', read)
 router.post("/product/create/:userId",requireSignin, isAuth,isAdmin,create);
 router.delete('/product/:productId/:userId',requireSignin, isAuth,isAdmin, remove);
 router.put('/product/:productId/:userId',requireSignin, isAuth,isAdmin, update);
+
+
+router.get('/product', list);
+
 
 router.param('userId', userById);
 router.param('ProductId', productById);
