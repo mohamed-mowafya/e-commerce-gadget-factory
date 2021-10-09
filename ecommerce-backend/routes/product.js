2cdc340b3b251d86df6db55e8a34b4cb4ea166a3
HamzaBelmouh <hamzabelmouh01@gmail.com>
2021-10-09 14:55:51 -0700

    Creation de la methode listRelated
    la methode listrelated va aller chercher les produits qui on la meme category dans la BD

--- ecommerce-backend/routes/product.js
+++ ecommerce-backend/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const {create, productById, read, remove, update, list} = require('../controllers/product');
+const {create, productById, read, remove, update, list, listRelated} = require('../controllers/product');
 const { requireSignin,isAdmin,isAuth} = require('../controllers/auth');
 const { userById} = require('../controllers/user');
 
@@ -13,6 +13,7 @@ router.put('/product/:productId/:userId',requireSignin, isAuth,isAdmin, update);
 
 
 router.get('/product', list);
+router.get('/products/related/:productId', listRelated),
 
 
 router.param('userId', userById);
