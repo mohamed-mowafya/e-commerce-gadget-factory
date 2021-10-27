a2745ba30fb9e48227b016bd2a0a5438da22ee1d
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-10-26 21:47:13 -0400

    Créer l'api de la modification du profile

---
+++ ecommerce-frontend/src/usagers/Profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../site/Layout";
+import { estAuthentifier } from "../Authentification";
+import { Link } from "react-router-dom";
+
+const Profile = () =>{
+    return (<h2>profile</h2>)
+}
+
+export default Profile;
