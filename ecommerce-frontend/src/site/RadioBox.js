f9ffd112b1dc13269574eabd1d8fc41d28f7a6c1
ViktoryaMkr <64432981+ViktoryaMkr@users.noreply.github.com>
2021-10-28 21:38:56 -0400

    Pouvoir filtrer par catégorie et en même temps par prix

--- ecommerce-frontend/src/site/RadioBox.js
+++ ecommerce-frontend/src/site/RadioBox.js
@@ -5,18 +5,17 @@ const RadioBox = ({ prices , handleFilters }) => {
     const [value, setValue] = useState(0);
 
     const handleChange = event => {
-     // -> a chaque changement dans input, sera envoyer au parent (Shop)
         handleFilters(event.target.value); //-> tous ce qu'on get sur le handelFilters qui vient de Shop(perent subcomponent)
         setValue(event.target.value); //-> update
     };
 
-
+ // -> a chaque changement dans input, sera envoyer au parent (Shop)
     return prices.map((p, i) => (
         <div key={i}>
             <input
                 onChange={handleChange}
                 value={`${p._id}`}
-                name={p}
+                name={p}  /* -> Permet de cocher 1 choix parmis les prix */
                 type="radio"
                 className="mr-2 ml-4"
             />
