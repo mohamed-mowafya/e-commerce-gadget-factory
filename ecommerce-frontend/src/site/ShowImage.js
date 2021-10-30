ac8340f2131013ed5c367e0245c6418beeab6651
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-10-30 18:01:33 -0400

    Fix le bug de l'image dans le frontend
    On peut maintenant afficher des images pour les produits

--- ecommerce-frontend/src/site/ShowImage.js
+++ ecommerce-frontend/src/site/ShowImage.js
@@ -5,7 +5,7 @@ import { API } from "../config";
 const ShowImage = ({ item, url }) => (
     <div className="product-img">
         <img
-            src={`${API}/${url}/photo/:${item._id}`} alt={item.name} className="mb-3 card-img-top"style={{ maxHeight: "100%", maxWidth: "100%" }}
+            src={`${API}/${url}/photo/${item._id}`} alt={item.name} className="mb-3 card-img-top"style={{ maxHeight: "100%", maxWidth: "100%" }}
         />
     </div>
 );
