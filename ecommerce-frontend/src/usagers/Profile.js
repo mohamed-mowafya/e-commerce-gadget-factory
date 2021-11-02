5e492034f7a51914d664fadebb2815a35639c5cc
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-11-02 18:43:21 -0400

    Implémentation des règles de changement de mot de passe

--- ecommerce-frontend/src/usagers/Profile.js
+++ ecommerce-frontend/src/usagers/Profile.js
@@ -8,16 +8,18 @@ const Profile = ({match}) =>{
         nom:"",
         prenom:"",
         email:"",
+        mdp:"",
+        mdp2:"",
         hashed_password:"",
         error:false,
         succes: false
-    });
+    }
+    );
     const {token} = estAuthentifier()
-
-    const {nom,prenom,email,hashed_password,error,succes} = values;
+    const [visible,setVisible] = useState(false);
+    var {nom,prenom,email,hashed_password,error,succes,mdp,mdp2} = values;
 
     const initaliser = (userId) =>{
-        console.log(userId +"ici")
         read(userId,token).then(data=>{
             if(data.error){
                 setValues({...values,error:true});
@@ -30,7 +32,6 @@ const Profile = ({match}) =>{
 
     const handleChange = name => (event) =>{
         setValues({...values,[name]: event.target.value},)
-
     }
 
     const modifierProfile = (nom,prenom,email,hashed_password) => (
@@ -41,15 +42,19 @@ const Profile = ({match}) =>{
         </div>
         <div className="form-group">
             <label className="text-muted">Prenom</label>
-            <input type="text" onChange={handleChange('prenom')} className="form-control" value={prenom}/>
+            <input required type="text" onChange={handleChange('prenom')} className="form-control" value={prenom}/>
         </div>
         <div className="form-group">
             <label className="text-muted">Email</label>
-            <input type="email" onChange={handleChange('email')} className="form-control" value={email}/>
+            <input required type="email" onChange={handleChange('email')} className="form-control" value={email}/>
         </div>
         <div className="form-group">
             <label className="text-muted">Mot de passe</label>
-            <input type="password" onChange={handleChange('hashed_password')} className="form-control" value={hashed_password}/>
+            <input id="mdp" type="password" onChange={handleChange('mdp')} className="form-control"/>
+        </div>
+        <div className="form-group">
+            <label className="text-muted">Confirmer mot de passe</label>
+            <input id="mdp2" type="password" onChange={handleChange('mdp2')} className="form-control"/>
         </div>
         <button onClick={envoyerInformations} className="btn btn-primary">Modifier</button>
         </form>
@@ -57,6 +62,8 @@ const Profile = ({match}) =>{
 
     const envoyerInformations = event =>{
         event.preventDefault()
+        verifierMDP()
+        if(!error){
         update(match.params.userId, token, {nom,prenom,email,hashed_password}).then(data=>{
             
             if(data.error){
@@ -64,11 +71,37 @@ const Profile = ({match}) =>{
             }
             else{
                 updateUser(data,()=>{
-                    setValues({...values,nom:data.nom,email:data.email,succes:true})
+                    setValues({...values,nom:data.nom,email:data.email})
                 })
             }
         })
+    }
+    }
 
+    const verifierMDP = () =>{
+        if(mdp == mdp2 && mdp.length>0){
+            // Source : https://regex101.com/r/dD5bF4/1
+            var regexMDP = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9_])");
+            if(!regexMDP.test(mdp)){
+                setVisible(true)
+                setValues({...values,error:true,succes:false})
+                document.getElementById("erreurMDP").innerHTML = "Votre mot de passe doit contenir au moins 8 caractères, "  
+                + "un chiffre et une lettre en majuscule."
+            }
+            else{
+                setVisible(false)
+                setValues({...values,error:false,succes:false})
+                hashed_password = mdp;
+            }
+        }
+        else{
+            
+                setValues({...values,error:true,succes:false})
+                setVisible(true)
+                document.getElementById("erreurMDP").innerHTML = "Les deux champs de mot de passe doivent être pareils."
+            
+        }
+        
     }
 
     const rediriger = (succes) =>{
@@ -76,6 +109,10 @@ const Profile = ({match}) =>{
             return <Redirect to="/"/>
         }
     }
+    const erreurMDP = () =>{
+        return <div id="erreurMDP" style={{display: visible ? "block" : "none"}}  class="alert alert-danger" role="alert"></div>
+    }
+
 
     useEffect(() => {
         initaliser(match.params.userId)
@@ -83,6 +120,7 @@ const Profile = ({match}) =>{
     return (
         <Layout title="Modification de profile" description="Modifier votre profile" className="container-fluid">
             <h2 className="mb-4">Modifier votre profile</h2>
+            {erreurMDP()}
             {modifierProfile(nom,prenom,email,hashed_password)}
             {rediriger(succes)}
         </Layout>
