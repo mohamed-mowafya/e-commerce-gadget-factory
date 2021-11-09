f63f368f993a334b978d08bc7ab5f6f982ca21c8
RedaKaramat <reda.karamat@gmail.com>
2021-11-09 15:28:50 -0500

    modification de l'interface usager pour la page inscription et connexion

--- ecommerce-frontend/src/usagers/Signup.js
+++ ecommerce-frontend/src/usagers/Signup.js
@@ -1,40 +1,111 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Layout from '../site/Layout';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../CSS/login_signup.css';
-import {signupAPI} from '../Authentification';
-const Signup = () =>{
-    const [valeursUsager,setValeursUsager] = useState({
+import { signupAPI } from '../Authentification';
+const Signup = () => {
+    const [valeursUsager, setValeursUsager] = useState({
         nom: '',
         prenom: '',
         email: '',
-        mdp : '',
-        erreur : '',
+        mdp: '',
+        erreur: '',
         succes: false
-    }); 
-useEffect(() => { document.body.style.backgroundColor = '#ededed' })
-const submitValeurs = (event) =>{
-    event.preventDefault(); // Methode qui permets d'interdire le refresh.
-    //setValeursUsager({...valeursUsager,erreur:false})
-    signupAPI({nom,prenom,email,mdp})
-    .then(data => {
-        if(data.error){
-            console.log(data.json + "data")
-            setValeursUsager(({...valeursUsager,erreur:data.error,succes:false}))
-            
-        }
-        else{
-            setValeursUsager({...valeursUsager,nom:'',prenom:'',mdp:'',email:'',erreur:'',succes:true})
-        }
-        
-    })
-};
-    const changementValeurs = contenu => event =>{
-        setValeursUsager({...valeursUsager,erreur:false, [contenu]: event.target.value});
+    });
+    useEffect(() => { document.body.style.backgroundColor = '#ededed' })
+    const submitValeurs = (event) => {
+        event.preventDefault(); // Methode qui permets d'interdire le refresh.
+        //setValeursUsager({...valeursUsager,erreur:false})
+        signupAPI({ nom, prenom, email, mdp })
+            .then(data => {
+                if (data.error) {
+                    console.log(data.json + "data")
+                    setValeursUsager(({ ...valeursUsager, erreur: data.error, succes: false }))
+
+                }
+                else {
+                    setValeursUsager({ ...valeursUsager, nom: '', prenom: '', mdp: '', email: '', erreur: '', succes: true })
+                }
+
+            })
+    };
+    const changementValeurs = contenu => event => {
+        setValeursUsager({ ...valeursUsager, erreur: false, [contenu]: event.target.value });
     }
-    const {nom,email,prenom,mdp,succes,erreur} = valeursUsager;
-    const form = () =>(
-        <div className="section-inscription">
+    const { nom, email, prenom, mdp, succes, erreur } = valeursUsager;
+    const form = () => (
+
+        <section className="vh-100" >
+            <div className="container h-100" >
+                <div className="row d-flex justify-content-center align-items-center h-100" >
+                    <div className="col-lg-12 col-xl-11" >
+                        <div className="card text-black" style={{ border: "none" }}>
+                            <div className="card-body p-md-5">
+                                <div className="row justify-content-center">
+                                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                                        <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Inscription</p>
+
+                                        <form action="POST" className="mx-1 mx-md-4">
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-signature fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <input onChange={changementValeurs('nom')} value={nom} type="text" id="form3Example1c" className="form-control" placeholder="Nom" />
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <input onChange={changementValeurs('prenom')} value={prenom} type="text" id="form3Example1c" className="form-control" placeholder="Prenom" />
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <input onChange={changementValeurs('email')} value={email} type="email" id="form3Example3c" className="form-control" placeholder="Adresse email" />
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <input onChange={changementValeurs('mdp')} value={mdp} type="password" id="form3Example4c" className="form-control" placeholder="Mot de passe" />
+
+                                                </div>
+                                            </div>
+
+
+                                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                                <button onClick={submitValeurs} type="button" className="btn btn-primary btn-lg"
+                                                    style={{ paddingLeft: "2.5rem;", paddingRight: "2.5rem;", backgroundColor: "#ed6436" }} onClick={submitValeurs}>S'inscrire</button>
+                                                
+                                            </div>
+
+                                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                                <p className="small fw-bold mt-2 pt-1 mb-0">Vous avez déja un compte? <a href="/login"
+                                                    className="link-danger">Connectez-vous</a></p>
+                                            </div>
+
+                                        </form>
+
+                                    </div>
+                                    <div className="col-md-9 col-lg-6 col-xl-7 align-items-center order-1 ">
+
+                                        <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png" className="img-fluid" alt="Sample image" />
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        /* <div className="section-inscription">
             <p className="header-inscription">Créer un compte x</p>
             <p className="paragraph-inscription">Créer un seul compte pour tous vos achats!</p>
         <form action="POST" className="flex-inscription">
@@ -55,27 +126,27 @@ const submitValeurs = (event) =>{
             <input onChange={changementValeurs('mdp')} type="password" className="form-control" value={mdp}/>
             </div>
             <button type="button" class="btn btn-dark btn-inscription" onClick={submitValeurs}>S'inscrire</button>
-        </form>
+       </form>
+        </div> */
+    )
+
+    const affichageErreur = () => (
+        <div className="alert alert-danger" style={{ display: erreur ? '' : 'none' }}>
+            {erreur}
         </div>
-        )
-        
-        const affichageErreur =  () =>(
-            <div className="alert alert-danger" style={{display: erreur ? '' : 'none'}}>
-                {erreur}
-            </div>
-        )
-        const affichageSucces = () => (
-            <div className="alert alert-success" style={{display: succes ? '' : 'none'}}>
-                Compte crée avec succès, veuillez vous connecter.
+    )
+    const affichageSucces = () => (
+        <div className="alert alert-success" style={{ display: succes ? '' : 'none' }}>
+            Compte crée avec succès, veuillez vous connecter.
         </div>
-        )
-        
+    )
+
     return (
-    <Layout title="Signup page" description="Ecommerce app" className="">
-        {affichageErreur()}
-        {affichageSucces()}
-        {form()}
-    </Layout>
+        <Layout title="Signup page" description="Ecommerce app" className="">
+            {affichageErreur()}
+            {affichageSucces()}
+            {form()}
+        </Layout>
     )
 }
 export default Signup;
