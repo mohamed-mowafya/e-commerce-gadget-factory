c2d825d35495228ebbcbfa49c4011e766aac2720
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-11-12 19:51:10 -0500

    Fix l'accueil

--- ecommerce-frontend/src/site/Home.js
+++ ecommerce-frontend/src/site/Home.js
@@ -1,18 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Layout from './Layout';
 import { getProducts } from "./apiSite";
 import Card from "./Card";
 import Search from "./Search";
 
 
-const Home = () =>{
+const Home = () => {
 
     const [productsBySell, setProductsBySell] = useState([]);
     const [productsByArrival, setProductsByArrival] = useState([]);
     const [error, setError] = useState(false); // false par defaut
-    
-    
-    
+
+
+
     const loadProductsBySell = () => {
         getProducts('sold').then(data => {
             if (data.error) {
@@ -24,7 +24,8 @@ const Home = () =>{
     };
 
     const loadProductsByArrival = () => {
-        getProducts('createdAt').then(data => {console.log(data);
+        getProducts('createdAt').then(data => {
+            console.log(data);
             if (data.error) {
                 setError(data.error);
             } else {
@@ -42,24 +43,32 @@ const Home = () =>{
 
     return (
         <Layout title="Home Page" description="Ecommerce app" className="container-fluid">
-            <h2 className="mb-4 couleur" >Meilleures ventes</h2>          
-            <div className="row">
-                {productsBySell.map((product,i) => (
-                    <div key={i} className="col-4 mb-3">
-                        <Card product={product} />
+            <h2 className="mb-4 couleur" >Meilleures ventes</h2>
+            <div className="col-12">
+                <div className="container d-flex justify-content-center mt-50 mb-50">
+                    <div className="row">
+                        {productsBySell.map((product, i) => (
+
+                            <Card product={product} />
+
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
 
-            <h2 className="mb-4">Les plus recents</h2>          
-            <div className="row"> 
-                {productsByArrival.map((product,i) => (
-                    <div key={i} className="col-4 mb-3">
-                        <Card product={product} />
+            <h2 className="mb-4">Les plus recents</h2>
+            <div className="col-12">
+                <div className="container d-flex justify-content-center mt-50 mb-50">
+                    <div className="row">
+                        {productsByArrival.map((product, i) => (
+
+                            <Card product={product} />
+
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         </Layout>
-        );
+    );
 };
 export default Home;
