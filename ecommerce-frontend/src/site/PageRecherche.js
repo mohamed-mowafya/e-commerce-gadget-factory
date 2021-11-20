4cd1d2c35ebc65cc9c38c9cb0fdae6d934bfecfb
momo-dg <64433872+momo-dg@users.noreply.github.com>
2021-11-19 20:34:22 -0500

    Amélioration de la page recherche

--- ecommerce-frontend/src/site/PageRecherche.js
+++ ecommerce-frontend/src/site/PageRecherche.js
@@ -4,26 +4,27 @@ import '../CSS/navbar.css';
 import Layout from "./Layout";
 import React, { useState, useEffect } from 'react';
 const PageRecherche = () =>{
-    const [erreur, setErreur] =  useState(false);
    const location = useLocation();
    const params = location.state.params;
    const results = params.results;
-   const search = params.search;
+    const search = params.search;
 
   
      const searchedProducts = (results = []) => {
         if(results.length == 0){
-           // setErreur(true);
+        
         }
         return (
             <div>
-                <h2 className="mt-4 mb-4">
-                </h2>
-                <div className="row">
+                <h2 className="d-flex justify-content-center mt-4">Resultats de recherche</h2>
+                <hr/>
+            <div className="col-9 ms-auto me-auto mt-4">
+                <div className="row d-flex justify-content-sm-between">
                     {results.map((product, i) => (
                         <Card key={i} product={product} />
                     ))}
                 </div>
+            </div>
             </div>
         );
     };
