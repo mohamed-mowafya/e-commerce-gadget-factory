0f2352e4120fe2442a57f9701446f8141ca1d8c5
ViktoryaMkr <64432981+ViktoryaMkr@users.noreply.github.com>
2021-11-26 01:55:15 -0500

    Ajout du service de courriel (API)
     - npm i @sendgrid/mail
    - permet au client et a l'administrateur de recevoir des courriel de confirmation d'achat et d'inscription
    
    Ajout de l'envoie du courriel pour l'inscription du client
    
    Ajout de l'envoie du courriel de confirmation de commande
    
    Ajout de l'envoie du courriel a l'admin,
    il a fallut 2 personne, car il y avait des bugs
    
    Co-Authored-By: med25ch <77120710+med25ch@users.noreply.github.com>

--- ecommerce-backend/controllers/orders.js
+++ ecommerce-backend/controllers/orders.js
@@ -1,15 +1,94 @@
 const { Commande, ItemChariot } = require('../models/commandes')
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+
+
+
+
 
 exports.create = (req, res) => {
-    req.body.order.user = req.profile
+    console.log('CREATE ORDER: ', req.body);
+    req.body.order.user = req.profile;
     const commande = new Commande(req.body.order)
     commande.save((error, data) => {
         if (error) {
             return res.status(400).json({
-            })
+                error: errorHandler(error)
+            });
         }
-        res.json(data)
-    })
+        console.log(req);
+        // User.find({ categories: { $in: categories } }).exec((err, users) => {}
+        console.log('ORDER IS JUST SAVED >>> ', commande);
+        // envoie une laerte de courriel a l'admin
+        // order.address
+        // order.products.length
+        // order.amount
+        const emailData = {
+            to: 'mkr.viktorya@gmail.com', // admin
+            from: 'viktomikto@gmail.com',
+            subject: `Une nouvelle commande est reçue`,
+            html: `
+            <h1>Salut administrateur, quelqu'un vient de faire un achat dans votre boutique en ligne</h1>
+            <h2>Nom du client: ${commande.user.nom}</h2>
+            <h2>Prénom du client: ${commande.user.prenom}</h2>
+            <h2>Adresse du client: ${commande.address}</h2>
+            <h2>Historique des achats de l'utilisateur: ${commande.user.history.length} purchase</h2>
+            <h2>Email: ${commande.user.email}</h2>
+            <h2>Total produits: ${commande.products.length}</h2>
+            <h2>Transaction ID: ${commande.transaction_id}</h2>
+            <h2>Status de commande: ${commande.statut}</h2>
+            <h2>Détails du produit:</h2>
+            <hr />
+            ${commande.products
+                .map(p => {
+                    return `<div>
+                        <h3>Nom du produit : ${p.name}</h3>
+                        <h3>Prix ​​du produit: ${p.price}</h3>
+                        <h3>La quantité de produit: ${p.count}</h3>
+                </div>`;
+                })
+                .join('--------------------')}
+            <h2>Coût total de la commande: ${commande.montant_total}<h2>
+            <p>Connectez-vous à votre tableau de bord</a> pour voir la commande en détail.</p>
+        `
+        };
+        sgMail
+            .send(emailData)
+            .then(sent => console.log('SENT >>>', sent))
+            .catch(err => console.log('ERR >>>', err));
+
+        // courriel envoyé au client ayant acheté un produit
+        const emailData2 = {
+            to: commande.user.email,
+            from: 'viktomikto@gmail.com',
+            subject: `Votre commande est en cours`,
+            html: `
+            <h1>Bonjour ${commande.user.nom}, merci de magasiner avec nous.</h1>
+            <h2>Produits au total: ${commande.products.length}</h2>
+            <h2> ID de transaction: ${commande.transaction_id}</h2>
+            <h2>Status de la commande: ${commande.statut}</h2>
+            <h2>Détails de produits:</h2>
+            <hr />
+            ${commande.products
+                .map(p => {
+                    return `<div>
+                        <h3>Nom du produit: ${p.name}</h3>
+                        <h3>Prix: ${p.price}</h3>
+                        <h3>Quantité: ${p.count}</h3>
+                </div>`;
+                })
+                .join('--------------------')}
+            <h2>Coût total: ${commande.montant_total}<h2>
+            <p>Merci :).</p>
+        `
+        };
+        sgMail
+            .send(emailData2)
+            .then(sent => console.log('Evoyé 2 >>>', sent))
+            .catch(err => console.log('Erreur 2 >>>', err));
+
+        res.json(data);
+    });
 };
 
 exports.afficherCommandes = (req, res) => {
@@ -55,3 +134,4 @@ exports.updateValeurEtat = (req, res) => {
         res.json(commande)
     })
 }
+
