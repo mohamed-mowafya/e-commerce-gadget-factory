b4abf31a3f91caa9144076cbdd8d9712e9a19890
ViktoryaMkr <64432981+ViktoryaMkr@users.noreply.github.com>
2021-12-09 18:03:59 -0500

    rendre le footer responsive

--- ecommerce-frontend/src/site/Footer.js
+++ ecommerce-frontend/src/site/Footer.js
@@ -5,27 +5,27 @@ const Footer = ()=>{
     return (
         
          
-        <div className="main-footer mt-5">
-            <div className="container">
+        <footer class="text-center text-lg-start text-white">
+            <div className="container p-4 pb-0 footerMargin">
                 <div className="row">
-                <div className="col">
-                <h4 className="h4Footer"> Pour nous contacter</h4>
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold h4Footer"> Pour nous contacter</h6>
                     <ul className="list-unstyled">
-                        <li className="liFooter"> (514)-535-4392</li>
+                        <li className="liFooter "> (514)-535-4392</li>
                         <li className="liFooter">LeGadgetsFactorie@compagnie.com</li>
                  
                     </ul>
                     </div>
-                <div className="col">
-                <h4 className="h4Footer"> Localisation</h4>
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold h4Footer"> Localisation</h6>
                 <ul className="list-unstyled">
                 <li className="liFooter"> H4N 1L4</li>
                 <li className="liFooter"> Montreal, Quebec  </li>
                 <li className="liFooter">10555 Ave de Bois-de-Boulogne</li>
                     </ul>
                 </div>
-                <div className="col">
-                <h4 className="h4Footer"> Membres</h4>
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold h4Footer"> Membres</h6>
                 <ul className="list-unstyled">
                 <li className="liFooter">Viktorya Mkrtchyan</li>
                 <li className="liFooter">Reda Karamat</li>
@@ -36,7 +36,7 @@ const Footer = ()=>{
                 </div>
                 <div className="row">
                 <p className="col-sm">
-                    <hr/>
+                    <hr />
                     &copy;{new Date().getFullYear()} Le Gadget's Factorie INC | Tout droits reservé
 
                 </p>
@@ -45,7 +45,7 @@ const Footer = ()=>{
 
             </div>
            
-        </div>
+        </footer>
      
        
     )
